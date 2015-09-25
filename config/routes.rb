e7a025809f7a07587b9be19b21f953faca0eedb8
choonggg <iamchoonggg@gmail.com>
2015-09-25 21:41:27 +0800

    generated user model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
+  get 'user/create'
+
+  get 'user/index'
+
+  get 'user/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
