e59e37b5519dbfec14e9c64744907dcc2b358fb1
choonggg <iamchoonggg@gmail.com>
2015-09-25 22:40:58 +0800

    generated User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+
+  def display_name
+    [first_name, last_name].compact.join(" ")
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,20 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe UserController, type: :controller do
-
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
+RSpec.describe UsersController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
@@ -26,6 +12,20 @@ RSpec.describe UserController, type: :controller do
   describe "GET #show" do
     it "returns http success" do
       get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #create" do
+    it "returns http success" do
+      get :create
       expect(response).to have_http_status(:success)
     end
   end
